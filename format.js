9a1c9fb4877735ccc0b0d38a2e622a2eadd7dc10
Tien Tran <tien@transfamily.info>
2021-02-18 21:22:23 +1100

    Support %o with stringifying superfluous argument objects

--- format.js
+++ format.js
@@ -1,22 +1,31 @@
+function tostring(arg) {
+  if (!arg || typeof arg != 'object') return arg;
+  if (typeof arg.toString == 'function' && !Array.isArray(arg)) return arg.toString();
+  return JSON.stringify(arg);
+}
+
 function format(fmt) {
-  var re = /(%?)(%([jds]))/g
+  var re = /(%?)(%([ojds]))/g
     , args = Array.prototype.slice.call(arguments, 1);
-  if(args.length) {
-    fmt = fmt.replace(re, function(match, escaped, ptn, flag) {
+  if (args.length) {
+    fmt = fmt.replace(re, function (match, escaped, ptn, flag) {
       var arg = args.shift();
-      switch(flag) {
+      switch (flag) {
         case 's':
           arg = '' + arg;
           break;
         case 'd':
           arg = Number(arg);
           break;
+        case 'o':
+          arg = tostring(arg);
+          break;
         case 'j':
           arg = JSON.stringify(arg);
           break;
       }
-      if(!escaped) {
-        return arg; 
+      if (!escaped) {
+        return arg;
       }
       args.unshift(arg);
       return match;
@@ -24,8 +33,8 @@ function format(fmt) {
   }
 
   // arguments remain after formatting
-  if(args.length) {
-    fmt += ' ' + args.join(' ');
+  if (args.length) {
+    fmt += ' ' + args.map(tostring).join(' ');
   }
 
   // update escaped %% values
